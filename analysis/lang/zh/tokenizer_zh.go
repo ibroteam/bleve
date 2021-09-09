e4267d52b4920919015cd296b8867dce0b84bc17
yinhao <tfzxyinhao@gmail.com>
2021-09-09 17:52:50 +0800

    add chinese support

---
+++ analysis/lang/zh/tokenizer_zh.go
@@ -0,0 +1,59 @@
+package zh
+
+import (
+	"github.com/blevesearch/bleve/v2/analysis"
+	"github.com/blevesearch/bleve/v2/registry"
+	"github.com/go-ego/gse"
+)
+
+const (
+	Name = "gse"
+)
+
+type GseTokenizer struct {
+	segmenter *gse.Segmenter
+}
+
+func NewGseTokenizer(dictFiles string) *GseTokenizer {
+	// segmenter := gse.New("./data/dict/zh/dict.txt", dictFiles)
+	// segmenter.MoreLog = false
+	// segmenter.SkipLog = true
+	var segmenter gse.Segmenter
+	segmenter.SkipLog = true
+	segmenter.LoadDict(dictFiles)
+	return &GseTokenizer{&segmenter}
+}
+
+/* func (t *GseTokenizer) Free()  {
+} */
+
+func (t *GseTokenizer) Tokenize(sentence []byte) analysis.TokenStream {
+	result := make(analysis.TokenStream, 0)
+	pos := 1
+	//segments := t.segmenter.ModeSegment(sentence, true)
+	segments := t.segmenter.Segment(sentence)
+	for _, seg := range segments {
+		token := analysis.Token{
+			Term:     []byte(seg.Token().Text()),
+			Start:    seg.Start(),
+			End:      seg.End(),
+			Position: pos,
+			Type:     analysis.Ideographic,
+		}
+		result = append(result, &token)
+		pos++
+	}
+	return result
+}
+
+func tokenizerConstructor(config map[string]interface{}, cache *registry.Cache) (analysis.Tokenizer, error) {
+	dicts, ok := config["user_dicts"].(string)
+	if !ok || dicts == "" {
+		panic("Initialize Gse tokenizer error: user dicts should NOT be empty")
+	}
+	return NewGseTokenizer(dicts), nil
+}
+
+func init() {
+	registry.RegisterTokenizer(Name, tokenizerConstructor)
+}
--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/blevesearch/zapx/v14 v14.2.3
 	github.com/blevesearch/zapx/v15 v15.2.3
 	github.com/couchbase/moss v0.1.0
+	github.com/go-ego/gse v0.69.2
 	github.com/golang/protobuf v1.3.2
 	github.com/kljensen/snowball v0.6.0
 	github.com/rcrowley/go-metrics v0.0.0-20190826022208-cac0b30c2563
